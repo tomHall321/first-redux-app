0c92523efa5d8d980498434b477db80aca1855ed
tom <tomhall4321@gmail.com>
2020-12-07 16:57:36 +0000

    works and not altered where files are

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+const App = ({player1, player2, onIncrementPlayer1, onIncrementPlayer2, handleReset, p1serving, winner}) => (
+  <>
+      {/* header */}
+      <header className="jumbotron mt-4 mb-0">
+          <h1>PongPing</h1>
       </header>
-    </div>
-  );
-}
+
+      {/* scores */}
+      <div className="row mb-4">
+          <div className="col-md-6 mt-4">
+              <div className={p1serving ? "card text-center bg-dark text-white" : "card text-center"}>
+                  <h5 className="card-header">Player 1</h5>
+                  <div className="card-body">
+                      <p className="card-text display-1">{player1}</p>
+                  </div>
+                  <div className="card-footer">
+                      <button 
+                        disabled={winner === 1 || winner === 2 ? true : false} 
+                        onClick={onIncrementPlayer1}
+                        className="form-control btn btn-success">+</button>
+                  </div>
+              </div>
+          </div>
+
+          <div className="col-md-6 mt-4">
+              <div className={!p1serving ? "card text-center bg-dark text-white" : "card text-center"}>
+                  <h5 className="card-header">Player 2</h5>
+                  <div className="card-body">
+                      <p className="card-text display-1">{player2}</p>
+                  </div>
+                  <div className="card-footer">
+                      <button
+                        disabled={winner === 1 || winner === 2 ? true : false}
+                        onClick={onIncrementPlayer2}
+                        className="form-control btn btn-success">+</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      { /* winner message */}
+      <h2 className={winner === 0 ? "d-none" : "alert alert-success"}>Player {winner} wins!</h2>
+      <hr />
+
+      { /* reset button */}
+      <button 
+        onClick={handleReset}
+        className="btn btn-danger">Reset</button>
+  </>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,100 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const initial = {
+  player1: 0,
+  player2: 0,
+  p1serving: true,
+  winner: 0,
+};
+
+//this is a reducer function
+const incrementPlayer1 = (state) => {
+  return {
+    ...state,
+    player1: state.player1 + 1,
+  };
+}
+
+const incrementPlayer2 = (state) => {
+  return {
+    ...state,
+    player2: state.player2 + 1,
+  };
+}
+
+const checkServer = (state) => {
+  return {
+    ...state,
+    p1serving: (state.player1 + state.player2) % 5 === 0 ? !state.p1serving : state.p1serving,
+  }
+}
+
+const winner = state => {
+  if (state.player1 >= 21 && state.player1 >= (state.player2 + 2)) {
+    return {
+      ...state,
+      winner: 1
+    }
+  } else if (state.player2 >= 21 && state.player2 >= (state.player1 + 2)) {
+    return {
+      ...state,
+      winner: 2
+    }
+  }
+  return { ...state, winner: 0 }
+}
+
+// const checkIfP1HasWon = (state) => {
+//   return {
+//     ...state,
+//     winner1: state.player1 >= 21 && state.player1 >= (state.player2 + 2) ? !state.winner1 : state.winner1,
+//   }
+// }
+
+// const checkIfP2HasWon = (state) => {
+//   return {
+//     ...state,
+//     winner2: state.player2 >= 21 && state.player2 >= (state.player1 + 2) ? !state.winner2 : state.winner2,
+//   }
+// }
+
+let reducer = (state, action) => {
+  switch (action.type){
+    case "INCREMENTPLAYER1": return winner(checkServer(incrementPlayer1(state)));
+    case "INCREMENTPLAYER2": return winner(checkServer(incrementPlayer2(state)));
+    case "RESET": return initial;
+    default: return state;
+  }
+}
+
+const store = createStore(
+  reducer,
+  initial,
+  window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let render = () => {
+  let state = store.getState();
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        player1={state.player1}
+        player2={state.player2}
+        onIncrementPlayer1={() => store.dispatch({type:"INCREMENTPLAYER1"})}
+        onIncrementPlayer2={() => store.dispatch({type:"INCREMENTPLAYER2"})}
+        handleReset={ () => store.dispatch({ type: "RESET" }) }
+        p1serving={state.p1serving}
+        winner={state.winner}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+render();
+store.subscribe(render); 
+//sets up subscription to store, so when the state updates render will be run again.
